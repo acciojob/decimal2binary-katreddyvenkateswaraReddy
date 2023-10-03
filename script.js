59830956f06f65efc93684c1ed55ecadf8bdb301
katreddyvenkateswaraReddy <63310358+katreddyvenkateswaraReddy@users.noreply.github.com>
2023-10-03 10:52:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function decimalToBinary(num) {
   //Write you code here
+	if (num === 0) {
+    return '0'; // Special case for 0
+  }
+
+  let binary = ''; // Initialize an empty string to store the binary representation
+
+  while (num > 0) {
+    const remainder = num % 2; // Calculate the remainder when dividing by 2
+    binary = remainder + binary; // Prepend the remainder to the binary string
+    num = Math.floor(num / 2); // Update num by dividing it by 2 (integer division)
+  }
+
+  return binary;
   
 }
 
